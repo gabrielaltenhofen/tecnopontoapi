f4eeef9f60aadd48e8e511a2b36583215f9d2f65
gabrielaltenhofen <ga0125419@setrem.com.br>
2023-10-07 20:03:10 -0300

    teste firebase

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const admin = require('firebase-admin');
 
+
 const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
